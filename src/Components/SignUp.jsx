7c52efa4df3d370ae326ec4a4f1be071b3ae1004
Rbng33 <128463403+Rbng33@users.noreply.github.com>
2023-05-04 11:53:26 +0100

    Add files via upload

---
+++ src/Components/SignUp.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Form from "./Form";
+import Navbar from "./Navbar";
+function SignUp() {
+  return (
+    <>
+    
+   
+      <div className="formulaire">
+        <Form />
+      </div>
+    </>
+  );
+}
+
+export default SignUp;
